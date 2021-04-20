8bc0cbc1095b7f2b0f2bca7ba6400b94c0d44e15
Kira Buckowing <82850293+kirabuckowing@users.noreply.github.com>
2021-04-19 22:53:14 -0500

    Update and rename iterator_size_comparison.py to Auto-iterator_size_comparison.py

--- Auto-iterator_size_comparison.py
+++ Auto-iterator_size_comparison.py
@@ -1,10 +1,12 @@
-#This program compares the size of two files - specifically, between original and resequenced files for my research, though it can be adapted beyond that as needed.
-#Relies on BioPython's built in FastQ iterator
+#This program compares the size of two files - specifically, between original and resequenced files for my research, since there were some sequencing errors encountered. 
+#It can be adapted beyond this as needed and is provided here for completeness - mostly used internally for determining next steps in the current research.
 
 # coding: utf-8
+#Relies on BioPython's built in FastQ iterator and gzip for accessing large, compressed files.
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
 import gzip
 
+##Set this to your desired file, listing the ID's you would like to iterate over within a directory.
 names = open('list.txt','r')
 lines = names.read().splitlines()
 for line in lines:
@@ -22,6 +24,7 @@ for line in lines:
             for i,l in enumerate(f1):
                 size1 = size1 + 1
         except:
+            #Alert if unexpected end of file is reached.
             print('UEOF')
         print('file 1 is '+str(size1)+' lines')
 
@@ -30,9 +33,9 @@ for line in lines:
             for i,l in enumerate(f2):
                 size2 = size2 + 1
         except:
+            #Alert if unexpected end of file is reached.
             print('UEOF')
         print('file 2 is '+str(size2)+' lines')
-
         if size1 > size2:
             count = 0
             print('file 1 is larger')
@@ -50,7 +53,22 @@ for line in lines:
                except:
                     count=count+1
 
-
+        elif size1 < size2:
+            count = 0
+            print('file 2 is larger')
+            for ID2_long, Seq2, Qual2 in iterator2:
+               try:
+                    ID1_long, Seq1, Qual1 = next(iterator1)
+                    file1.write('@' + ID1_long + '\n')
+                    file1.write(Seq1 + '\n')
+                    file1.write('+ \n')
+                    file1.write(Qual1 + '\n')
+                    file2.write('@' + ID2_long + '\n')
+                    file2.write(Seq2 + '\n')
+                    file2.write('+ \n')
+                    file2.write(Qual2 + '\n')
+               except:
+                    count=count+1
         else:
             count = 0
             print('Files are the same length')
