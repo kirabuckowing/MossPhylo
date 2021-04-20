a32f6a0e58802c5488a2c5fd9c8c8ff9eef5fc29
Kira Buckowing <82850293+kirabuckowing@users.noreply.github.com>
2021-04-19 21:57:07 -0500

    Update slack_done_call.sh

--- slack_done_call.sh
+++ slack_done_call.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#This program uses a slack webhook to send a message to a channel there set up with the right app, letting you know whenever this bash file is called. 
-#Used within long jobs to alert the user when a portion of such finished (such as a single ID running through trinity or transdecoder, which could take hours and often needed to be run in the background, as through a screen).
+#This program uses a slack webhook to send a message to a channel there (after it has been set up with the right app), letting you know whenever this bash file is called. 
+#Used within long jobs to alert the user when a portion of such finished (such as a single ID running through trinity or transdecoder, which could take hours and often needs to be run in the background, as through a screen).
 
 #webhook_url=https://hooks.slack.com/services/APPCODE
 
-curl -X POST -H 'Content-type: application/json' --data '{"text": "YOUR JOB/DETAILS has finished."}' https://hooks.slack.com/services/APPCODE
+curl -X POST -H 'Content-type: application/json' --data '{"text": "YOUR JOB/AN ID RUN/ETC has finished."}' https://hooks.slack.com/services/APPCODE
